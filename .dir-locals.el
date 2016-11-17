6f686c16f2154fe5d916a712c7e120f48b5c9a6e
Joe Jevnik <joe@quantopian.com>
2016-11-16 20:40:19 -0500

    DEV: update dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,15 +1,15 @@
 ((c-mode . ((mode . c++)
             (eval . (setq flycheck-gcc-include-path
-                          '("include"
-                            "../"
+                          '("../"
                             "../include"
+                            "../../include"
                             "/usr/include/python3.5m"
                             "../gtest/googletest/include")))
             (eval . (c-set-offset 'innamespace 0))))
  (c++-mode . ((eval . (setq flycheck-gcc-include-path
-                            '("include"
-                              "../"
+                            '("../"
                               "../include"
+                              "../../include"
                               "/usr/include/python3.5m"
                               "../gtest/googletest/include")))
               (eval . (c-set-offset 'innamespace 0))
