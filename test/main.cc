5e21938856cc9ecd3701dfa79da528cbba28fd63
Joe Jevnik <joe@quantopian.com>
2016-11-16 20:40:05 -0500

    TST: add gdb helper script from python

--- test/main.cc
+++ test/main.cc
@@ -2,13 +2,15 @@
 
 #include "gtest/gtest.h"
 
+// gdb doesn't see the PyLongObject type unless we have a variable with this
+// type somewhere in the binary.
+static PyLongObject _gdb_helper __attribute__((unused));
 
 int main(int argc, char **argv) {
-  int ret;
-
-  testing::InitGoogleTest(&argc, argv);
-  Py_Initialize();
-  ret = RUN_ALL_TESTS();
-  Py_Finalize();
-  return ret;
+    int ret;
+    testing::InitGoogleTest(&argc, argv);
+    Py_Initialize();
+    ret = RUN_ALL_TESTS();
+    Py_Finalize();
+    return ret;
 }
